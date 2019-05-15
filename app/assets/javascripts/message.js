5d70f5e943809df63ada0c978f24e0738b2e8468
TakashiKawabata <batashi527@gmail.com>
2019-05-15 16:41:41 +0900

    新ブランチcommit3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,27 @@
 $(function(){
   function buildHTML(message){
-      if ( message.image ) {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
+    message.text ? temptext = message.content : temptext = ""
+    message.image ? tempimage = message.image : tempimage = ""
+    var html =
+      `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+           <div class="upper-message__user-name">
+            ${message.user_name}
+           </div>
+            <div class="upper-message__date">
+             ${message.date}
             </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
-    }
+           </div>
+           <div class="lower-message">
+            <p class="lower-message__content">
+              ${temptext}
+            </p>
+         </div>
+          <img src=${tempimage}>
+       </div>`
+      return html;
+      } 
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -58,7 +41,6 @@ $(function(){
      $('form')[0].reset(); 
     })
     .fail(function(data){
-      console.log()
       alert('error');
     });
     return false;
